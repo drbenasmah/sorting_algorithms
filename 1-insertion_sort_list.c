45b25d09a71b925ccca7031b969d890d77ed4810
drbenasmah <drbenasmah2016@gmail.com>
2023-04-24 20:11:37 +0200

    task 1 update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,33 +10,33 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+listint_t *current, *temp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
 
-    current = (*list)->next;
+current = (*list)->next;
 
-    while (current != NULL)
-    {
-        temp = current;
+while (current != NULL)
+{
+temp = current;
 
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-            temp->prev->next = temp->next;
+while (temp->prev != NULL && temp->n < temp->prev->n)
+{
+temp->prev->next = temp->next;
 
-            if (temp->next != NULL)
-                temp->next->prev = temp->prev;
+if (temp->next != NULL)
+temp->next->prev = temp->prev;
 
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
+temp->next = temp->prev;
+temp->prev = temp->prev->prev;
+temp->next->prev = temp;
 
-            if (temp->prev == NULL)
-                *list = temp;
+if (temp->prev == NULL)
+*list = temp;
 
-            print_list(*list);
-        }
+print_list(*list);
+}
 
 current = current->next;
 }
