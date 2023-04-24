982528d77f38f4da66030d799439c2cd9e48e63b
drbenasmah <drbenasmah2016@gmail.com>
2023-04-24 21:11:44 +0200

    task 2 update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,14 +1,29 @@
 #include "sort.h"
+
+/**
+ * swap - swaps two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 /**
  * selection_sort - sorts an array of integers in ascending order
  *                  using the Selection sort algorithm
- *
- * @array: the array to sort
- * @size: the size of the array
+ * @array: pointer to the array to be sorted
+ * @size: size of the array
  */
 void selection_sort(int *array, size_t size)
 {
     size_t i, j, min_idx;
+
+    if (array == NULL || size < 2)
+        return;
 
     for (i = 0; i < size - 1; i++)
     {
@@ -18,10 +33,11 @@ void selection_sort(int *array, size_t size)
             if (array[j] < array[min_idx])
                 min_idx = j;
         }
+
         if (min_idx != i)
         {
-            swap(&array[min_idx], &array[i]);
+            swap(&array[i], &array[min_idx]);
             print_array(array, size);
-}
-}
+        }
+    }
 }
